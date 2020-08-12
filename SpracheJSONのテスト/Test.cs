dbcc7a9cb69744b9687b5ef243a011ae4a18d84d
新田光宏 <potimarimo@gmail.com>
2020-08-13 02:07:22 +0900

    SprachJSONのテストを動くようにする

---
+++ SpracheJSONのテスト/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using SpracheJSON;
+
+namespace SpracheJSONのテスト
+{
+    namespace JSONテスト
+    {
+        public class JsonParserのテスト
+        {
+            [Fact]
+            public void 空のオブジェクトを識別します()
+            {
+                var result = JSON.Parse("{}");
+
+                result.Pairs.Count.Is(0);
+            }
+        }
+    }
+
+}

e9edbda87cbba9cfe2baca0de07dedbf9e157818
新田光宏 <potimarimo@gmail.com>
2020-08-16 22:51:40 +0900

    リファクタリング

--- JsonParser/JSON.cs
+++ JsonParser/JSON.cs
@@ -1,6 +1,7 @@
 ﻿using Marimo.ParserCombinator;
 using System;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,14 +14,24 @@ namespace Marimo.Parser
 
         static Parser<char> bracketOpen => new CharParser('{');
 
-        static Parser<char> bracketClose => new CharParser('{');
+        static Parser<char> bracketClose => new CharParser('}');
 
-        static Parser<(char, char)> jsonObject => SequenceParser.Create(bracketOpen, bracketClose);
+        static Parser<JSONObject> jsonObject =>
+            ParserConverter.Create(
+                SequenceParser.Create(bracketOpen, bracketClose),
+                _=> new JSONObject());
 
-        public static async Task<JSONObject> ParseAsync(string parsed)
+        public static async Task<JSONObject> ParseAsync(string text)
         {
-            var (isSuccess, _, _) = await jsonObject.ParseAsync(new Cursol(parsed));
-            return new JSONObject();
+            var (isSuccess, _, parsed) = await jsonObject.ParseAsync(new Cursol(text));
+            if(isSuccess)
+            {
+                return parsed;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }

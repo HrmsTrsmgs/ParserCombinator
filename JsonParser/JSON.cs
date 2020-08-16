8f6b82704a44a9bf5f24770baf899d869c75437d
新田光宏 <potimarimo@gmail.com>
2020-08-16 21:37:49 +0900

    Marimo.Parser.JSONのリファクタリング

--- JsonParser/JSON.cs
+++ JsonParser/JSON.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace Marimo.Parser
 {
-
+    
     public class JSON
     {
-        public static Parser<string> Null => new WordParser("null", true);
+        static Parser<string> Null => new WordParser("null", true);
+
+        static Parser<char> bracketOpen => new CharParser('{');
+
+        static Parser<char> bracketClose => new CharParser('{');
+
+        static Parser<(char, char)> jsonObject => SequenceParser.Create(bracketOpen, bracketClose);
 
         public static async Task<JSONObject> ParseAsync(string parsed)
         {
+            var (isSuccess, _, _) = await jsonObject.ParseAsync(new Cursol(parsed));
             return new JSONObject();
         }
     }

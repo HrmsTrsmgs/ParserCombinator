49314906be1af1d508741e8325aa626b59c2c0f9
新田光宏 <potimarimo@gmail.com>
2020-08-16 22:05:42 +0900

    ParserCombinatorの作成

---
+++ ParserCombinator/ParserConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.ParserCombinator
+{
+    public static class ParserConverter
+    {
+        public static ParserConverter<T, U> Create<T, U>(Parser<U> parser, Func<U, T> converter)
+        {
+            return new ParserConverter<T, U>(parser, converter);
+        }
+    }
+    public class ParserConverter<T, U> : Parser<T>
+    {
+        Parser<U> parser { get; }
+        Func<U, T> converter { get; }
+
+        public ParserConverter(Parser<U> parser, Func<U, T> converter)
+        {
+            this.parser = parser;
+            this.converter = converter;
+        }
+
+        public override async Task<(bool isSuccess, Cursol cursol, T parsed)> ParseAsync(Cursol cursol)
+        {
+            return (false, cursol, default(T));
+        }
+    }
+}
---
+++ ParserCombinatorのテスト/ParserConverterのテスト.cs
@@ -0,0 +1,18 @@
+﻿using Marimo.ParserCombinator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParserCombinatorのテスト
+{
+    public class ParserConverterのテスト
+    {
+        [Fact]
+        public async Task パースします()
+        {
+            var tested = ParserConverter.Create(new WordParser("123"), s => int.Parse(s));
+        }
+    }
+}

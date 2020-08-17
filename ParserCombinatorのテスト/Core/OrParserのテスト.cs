9711cac8a652ca12b4e94b635eb2c4064e799c07
新田光宏 <potimarimo@gmail.com>
2020-08-18 03:09:18 +0900

    OrParserの一つ目の要素のパース結果

--- ParserCombinatorのテスト/Core/OrParserのテスト.cs
+++ ParserCombinatorのテスト/Core/OrParserのテスト.cs
@@ -18,5 +18,15 @@ namespace Marimo.Test.ParserCombinator.Core
 
             isSuccess.IsTrue();
         }
+
+        [Fact]
+        public async Task 一つ目の要素のパースに成功したら一つ目の要素が返ります()
+        {
+            var tested = new OrParser<char>(new CharParser('a'), new CharParser('b'));
+
+            var (_, _, parsed) = await tested.ParseAsync(new Cursol("a"));
+
+            parsed.Is('a');
+        }
     }
 }

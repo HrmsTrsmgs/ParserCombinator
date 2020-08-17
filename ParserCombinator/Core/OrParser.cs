dfbefb1a5cf0fb1fc185cc9abebcb472421be4cb
新田光宏 <potimarimo@gmail.com>
2020-08-18 03:06:13 +0900

    OrParserの一つ目の要素のパースのテスト

---
+++ ParserCombinator/Core/OrParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.ParserCombinator.Core
+{
+    public class OrParser<T> : IParser<T>
+    {
+        IParser<T> parser1 { get; }
+        IParser<T> parser2 { get; }
+        public OrParser(IParser<T> parser1, IParser<T> parser2)
+        {
+            this.parser1 = parser1;
+            this.parser2 = parser2;
+        }
+
+        public async Task<(bool isSuccess, Cursol cursol, T parsed)> ParseAsync(Cursol cursol)
+        {
+            return await  parser1.ParseAsync(cursol);
+        }
+    }
+}
---
+++ ParserCombinatorのテスト/Core/OrParserのテスト.cs
@@ -0,0 +1,22 @@
+﻿using Marimo.ParserCombinator.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marimo.Test.ParserCombinator.Core
+{
+    public class OrParserのテスト
+    {
+        [Fact]
+        public async Task 一つ目の要素のパースに成功したら成功です()
+        {
+            var tested = new OrParser<char>(new CharParser('a'), new CharParser('b'));
+
+            var (isSuccess, _, _) = await tested.ParseAsync(new Cursol("a"));
+
+            isSuccess.IsTrue();
+        }
+    }
+}

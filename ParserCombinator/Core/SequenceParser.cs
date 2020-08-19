8f4b5f5c766f06c186a4d151e65f07f69e777b4b
新田光宏 <potimarimo@gmail.com>
2020-08-19 23:49:55 +0900

    要素を6つ持つSeqenceParserを追加

--- ParserCombinator/Core/SequenceParser.cs
+++ ParserCombinator/Core/SequenceParser.cs
@@ -44,6 +44,37 @@ namespace Marimo.ParserCombinator.Core
                 : (false, cursol, default);
         }
     }
+    public class SequenceParser<T1, T2, T3, T4, T5, T6> : IParser<(T1, T2, T3, T4, T5, T6)>
+    {
+        (IParser<T1>, IParser<T2>, IParser<T3>, IParser<T4>, IParser<T5>, IParser<T6>) parsers { get; }
+        public SequenceParser(
+            IParser<T1> parser1,
+            IParser<T2> parser2,
+            IParser<T3> parser3,
+            IParser<T4> parser4,
+            IParser<T5> parser5,
+            IParser<T6> parser6)
+        {
+            this.parsers = (parser1, parser2, parser3, parser4, parser5, parser6);
+        }
+
+        public async Task<(bool isSuccess, Cursol cursol, (T1, T2, T3, T4, T5, T6) parsed)> ParseAsync(Cursol cursol)
+        {
+            (T1, T2, T3, T4, T5, T6) returnValue = default;
+            var helper = new SequenceHelper(cursol);
+
+            return
+                await helper.ParseAsync(parsers.Item1, value => returnValue.Item1 = value) &&
+                await helper.ParseAsync(parsers.Item2, value => returnValue.Item2 = value) &&
+                await helper.ParseAsync(parsers.Item3, value => returnValue.Item3 = value) &&
+                await helper.ParseAsync(parsers.Item4, value => returnValue.Item4 = value) &&
+                await helper.ParseAsync(parsers.Item5, value => returnValue.Item5 = value) &&
+                await helper.ParseAsync(parsers.Item6, value => returnValue.Item6 = value)
+                ? (true, helper.Current, returnValue)
+                : (false, cursol, default);
+        }
+    }
+
     public class SequenceParser<T1, T2, T3, T4, T5, T6, T7> : IParser<(T1, T2, T3, T4, T5, T6, T7)>
     {
         (IParser<T1>, IParser<T2>, IParser<T3>, IParser<T4>, IParser<T5>, IParser<T6>, IParser<T7>) parsers { get; }

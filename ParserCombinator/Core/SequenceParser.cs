82786ddc758ad023dffeb1452548ffab2ced6d74
新田光宏 <potimarimo@gmail.com>
2020-08-19 22:54:20 +0900

    要素を9つ持つSeqenceParserの追加

--- ParserCombinator/Core/SequenceParser.cs
+++ ParserCombinator/Core/SequenceParser.cs
@@ -44,6 +44,42 @@ namespace Marimo.ParserCombinator.Core
                 : (false, cursol, default);
         }
     }
+    public class SequenceParser<T1, T2, T3, T4, T5, T6, T7, T8, T9> : IParser<(T1, T2, T3, T4, T5, T6, T7, T8, T9)>
+    {
+        (IParser<T1>, IParser<T2>, IParser<T3>, IParser<T4>, IParser<T5>, IParser<T6>, IParser<T7>, IParser<T8>, IParser<T9>) parsers { get; }
+        public SequenceParser(
+            IParser<T1> parser1,
+            IParser<T2> parser2,
+            IParser<T3> parser3,
+            IParser<T4> parser4,
+            IParser<T5> parser5,
+            IParser<T6> parser6,
+            IParser<T7> parser7,
+            IParser<T8> parser8,
+            IParser<T9> parser9)
+        {
+            this.parsers = (parser1, parser2, parser3, parser4, parser5, parser6, parser7, parser8, parser9);
+        }
+
+        public async Task<(bool isSuccess, Cursol cursol, (T1, T2, T3, T4, T5, T6, T7, T8, T9) parsed)> ParseAsync(Cursol cursol)
+        {
+            (T1, T2, T3, T4, T5, T6, T7, T8, T9) returnValue = default;
+            var helper = new SequenceHelper(cursol);
+
+            return
+                await helper.ParseAsync(parsers.Item1, value => returnValue.Item1 = value) &&
+                await helper.ParseAsync(parsers.Item2, value => returnValue.Item2 = value) &&
+                await helper.ParseAsync(parsers.Item3, value => returnValue.Item3 = value) &&
+                await helper.ParseAsync(parsers.Item4, value => returnValue.Item4 = value) &&
+                await helper.ParseAsync(parsers.Item5, value => returnValue.Item5 = value) &&
+                await helper.ParseAsync(parsers.Item6, value => returnValue.Item6 = value) &&
+                await helper.ParseAsync(parsers.Item7, value => returnValue.Item7 = value) &&
+                await helper.ParseAsync(parsers.Item8, value => returnValue.Item8 = value) &&
+                await helper.ParseAsync(parsers.Item9, value => returnValue.Item9 = value)
+                ? (true, helper.Current, returnValue)
+                : (false, cursol, default);
+        }
+    }
     public class SequenceParser<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : IParser<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)>
     {
         (IParser<T1>, IParser<T2>, IParser<T3>, IParser<T4>, IParser<T5>, IParser<T6>, IParser<T7>, IParser<T8>, IParser<T9>, IParser<T10>) parsers { get; }

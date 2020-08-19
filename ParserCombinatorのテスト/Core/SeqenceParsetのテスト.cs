f694fec77d39977d4f592411dc7bc5403c992648
新田光宏 <potimarimo@gmail.com>
2020-08-19 21:59:54 +0900

    テストのリファクタリング

--- ParserCombinatorのテスト/Core/SeqenceParsetのテスト.cs
+++ ParserCombinatorのテスト/Core/SeqenceParsetのテスト.cs
@@ -8,13 +8,21 @@ using Xunit;
 
 namespace Marimo.Test.ParserCombinator.Core
 {
-    public class 要素を二つ持つSeqenceParserのテスト
+    public class 要素を2つ持つSeqenceParserのテスト
     {
+        IParser<(string, string)> tested;
+
+        public 要素を2つ持つSeqenceParserのテスト()
+        {
+            tested = new SequenceParser<string, string>(
+                new WordParser("public"),
+                new WordParser("static"));
+        }
+
         [Fact]
         public async Task ParseAsyncは指定した単語を読み込みに成功します()
         {
             var cursol = new Cursol("public static");
-            var tested = SequenceParser.Create(new WordParser("public"), new WordParser("static"));
 
             var result = await tested.ParseAsync(cursol);
 
@@ -25,7 +33,6 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncは一つ目の解析に失敗した場合は失敗します()
         {
             var cursol = new Cursol("publica static");
-            var tested = SequenceParser.Create(new WordParser("public"), new WordParser("static"));
 
             var result = await tested.ParseAsync(cursol);
 
@@ -36,7 +43,6 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncは指定した単語を読み込みます()
         {
             var cursol = new Cursol("public static");
-            var tested = SequenceParser.Create(new WordParser("public"), new WordParser("static"));
 
             var result = await tested.ParseAsync(cursol);
 
@@ -47,7 +53,6 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncはふたつ目の解析に失敗した場合は失敗します()
         {
             var cursol = new Cursol("public statiac");
-            var tested = SequenceParser.Create(new WordParser("public"), new WordParser("static"));
 
             var result = await tested.ParseAsync(cursol);
 
@@ -65,17 +70,24 @@ namespace Marimo.Test.ParserCombinator.Core
             result.cursol.Index.Is("public static".Length);
         }
     }
-    public class 要素を三つ持つSeqenceParserのテスト
+    public class 要素を3つ持つSeqenceParserのテスト
     {
+        IParser<(string, string, string)> tested;
+
+        public 要素を3つ持つSeqenceParserのテスト()
+        {
+            tested = new SequenceParser<string, string, string>(
+                new WordParser("public"),
+                new WordParser("static"),
+                new WordParser("int"));
+        }
+
+
         [Fact]
         public async Task ParseAsyncは指定した単語を読み込みに成功します()
         {
             var cursol = new Cursol("public static int");
-            var tested = SequenceParser.Create(
-                new WordParser("public"),
-                new WordParser("static"),
-                new WordParser("int"));
-
+            
             var result = await tested.ParseAsync(cursol);
 
             result.isSuccess.IsTrue();
@@ -85,10 +97,7 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncは一つ目の解析に失敗した場合は失敗します()
         {
             var cursol = new Cursol("publica static int");
-            var tested = SequenceParser.Create(
-                new WordParser("public"),
-                new WordParser("static"),
-                new WordParser("int"));
+
             var result = await tested.ParseAsync(cursol);
 
             result.isSuccess.IsFalse();
@@ -98,10 +107,7 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncは指定した単語を読み込みます()
         {
             var cursol = new Cursol("public static int");
-            var tested = SequenceParser.Create(
-                new WordParser("public"),
-                new WordParser("static"),
-                new WordParser("int"));
+
             var result = await tested.ParseAsync(cursol);
 
             result.parsed.Is(("public", "static", "int"));
@@ -111,10 +117,7 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncはふたつ目の解析に失敗した場合は失敗します()
         {
             var cursol = new Cursol("public statiac int");
-            var tested = SequenceParser.Create(
-                new WordParser("public"),
-                new WordParser("static"),
-                new WordParser("int"));
+
             var result = await tested.ParseAsync(cursol);
 
             result.isSuccess.IsFalse();
@@ -123,10 +126,7 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncは三つ目の解析に失敗した場合は失敗します()
         {
             var cursol = new Cursol("public statiac innt");
-            var tested = SequenceParser.Create(
-                new WordParser("public"),
-                new WordParser("static"),
-                new WordParser("int"));
+
             var result = await tested.ParseAsync(cursol);
 
             result.isSuccess.IsFalse();
@@ -137,10 +137,6 @@ namespace Marimo.Test.ParserCombinator.Core
         public async Task ParseAsyncは読み込みに成功した場合に単語の長さだけ進んだカーソルを返します()
         {
             var cursol = new Cursol("public static int");
-            var tested = SequenceParser.Create(
-                new WordParser("public"),
-                new WordParser("static"),
-                new WordParser("int"));
 
             var result = await tested.ParseAsync(cursol);
 

d9cf0f65cf5302274228528c8d51d92583bdfea3
新田光宏 <potimarimo@gmail.com>
2020-08-21 05:12:33 +0900

    ZeroOrMoreParserの追加

---
+++ ParserCombinator/Core/ZeroOrMoreParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.ParserCombinator.Core
+{
+    public class ZeroOrMoreParser<T> : IParser<IEnumerable<T>>
+    {
+        IParser<T> parser;
+
+        public ZeroOrMoreParser(IParser<T> parser)
+        {
+            this.parser = parser;
+        }
+
+        public async Task<(bool isSuccess, Cursol cursol, IEnumerable<T> parsed)> ParseAsync(Cursol cursol)
+        {
+            return (true, cursol, null);
+        }
+    }
+}
---
+++ ParserCombinatorのテスト/Core/ZeroOrMoreParserのテスト.cs
@@ -0,0 +1,22 @@
+﻿using Marimo.ParserCombinator.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marimo.Test.ParserCombinator.Core
+{
+    public class ZeroOrMoreParserのテスト
+    {
+        [Fact]
+        public async Task 一つ目の要素のパースに失敗しても成功です()        {
+            var tested = new ZeroOrMoreParser<char>(new CharParser('a'));
+
+            var (isSuccess, _, _) = await tested.ParseAsync(new Cursol("b"));
+
+            isSuccess.IsTrue();
+        }
+    }
+}

af4ecd6677b9806a334a2f05bf2b455ae125b1eb
新田光宏 <potimarimo@gmail.com>
2020-08-20 23:52:05 +0900

    OneOrMoreParserの失敗機能を実装

--- ParserCombinatorのテスト/Core/OneOrMoreParserのテスト.cs
+++ ParserCombinatorのテスト/Core/OneOrMoreParserのテスト.cs
@@ -10,6 +10,15 @@ namespace Marimo.Test.ParserCombinator.Core
     public class OneOrMoreParserのテスト
     {
         [Fact]
+        public async Task 一つ目の要素のパースに失敗したら失敗です()        {
+            var tested = new OrParser<char>(new CharParser('a'));
+
+            var (isSuccess, _, _) = await tested.ParseAsync(new Cursol("b"));
+
+            isSuccess.IsFalse();
+        }
+
+        [Fact]
         public async Task 一つ目の要素のパースに成功したら成功です()
         {
             var tested = new OrParser<char>(new CharParser('a'));

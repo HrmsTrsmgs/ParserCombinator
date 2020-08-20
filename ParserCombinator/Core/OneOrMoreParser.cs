409dd5cedd2d3dcd6e436c2732546890355bf8ff
新田光宏 <potimarimo@gmail.com>
2020-08-20 23:50:26 +0900

    OneOrMoreParserを追加

---
+++ ParserCombinator/Core/OneOrMoreParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.ParserCombinator.Core
+{
+    public class OneOrMoreParser<T> : IParser<IEnumerable<T>>
+    {
+        public async Task<(bool isSuccess, Cursol cursol, IEnumerable<T> parsed)> ParseAsync(Cursol cursol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

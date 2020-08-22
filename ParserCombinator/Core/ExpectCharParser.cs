8fd559d86c10664ca36e552746559496051dbf5a
新田光宏 <potimarimo@gmail.com>
2020-08-22 15:27:19 +0900

    ExpectCharParserの追加

---
+++ ParserCombinator/Core/ExpectCharParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.ParserCombinator.Core
+{
+    public class ExpectCharParser : IParser<char>
+    {
+        IParser<char> ecpectChars { get; }
+
+        public ExpectCharParser(IParser<char> ecpectChars)
+        {
+            this.ecpectChars = ecpectChars;
+        }
+
+        public async Task<(bool isSuccess, Cursol cursol, char parsed)> ParseAsync(Cursol cursol)
+        {
+            return (true, cursol, default);
+        }
+    }
+}
---
+++ ParserCombinatorのテスト/Core/ExpectCharParserのテスト.cs
@@ -0,0 +1,24 @@
+﻿using Marimo.ParserCombinator.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marimo.ParserCombinator.Test.Core
+{
+    public class ExpectCharParserのテスト
+    {
+        [Fact]
+        public async Task ParseAsyncは指定した文字を読み込みに成功します()
+        {
+            var cursol = new Cursol("public");
+            var tested = new ExpectCharParser(
+                new CharParser('a'));
+
+            var result = await tested.ParseAsync(cursol);
+
+            result.isSuccess.IsTrue();
+        }
+    }
+}

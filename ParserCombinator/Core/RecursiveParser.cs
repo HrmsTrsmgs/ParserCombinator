c5bb330d25325ff1af04dadcb514d3848f75cc85
新田光宏 <potimarimo@gmail.com>
2020-08-24 03:07:09 +0900

    RecursiveParserの実装

---
+++ ParserCombinator/Core/RecursiveParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marimo.ParserCombinator.Core
+{
+    public class RecursiveParser<T> : IParser<T>
+    {
+        Func<T> parserGetter { get; }
+        public RecursiveParser(Func<T> parserGetter)
+        {
+            this.parserGetter = parserGetter;
+        }
+
+        public Task<(bool isSuccess, Cursol cursol, T parsed)> ParseAsync(Cursol cursol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParserCombinatorのテスト/Core/RecursiveParserのテスト.cs
@@ -0,0 +1,22 @@
+﻿using Marimo.ParserCombinator.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marimo.ParserCombinator.Test.Core
+{
+    public class RecursiveParserのテスト
+    {
+        [Fact]
+        public async Task ParseAsyncは内部パーサーと同じように成功します()
+        {
+            var cursol = new Cursol("public");
+            var parser = new CharParser('p');
+            var tested = new RecursiveParser<char>(() => parser);
+            (await tested.ParseAsync(cursol)).IsStructuralEqual(await parser.ParseAsync(cursol));
+        }
+
+    }
+}

9d289544cc21ec1bad23afdca09f362e771a7dac
新田光宏 <potimarimo@gmail.com>
2020-08-26 07:09:22 +0900

    ParserConbinatorParormanceの作成

---
+++ PaformanceTest/Test.cs
@@ -0,0 +1,22 @@
+﻿using Marimo.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParormanceTest
+{
+    public class Test
+    {
+        [Fact]
+        public async Task パフォーマンス測定()
+        {
+            string text = File.ReadAllText(@"..\..\testdata\test.json");
+
+            await JSON.ParseAsync(text);
+        }
+    }
+}
---
+++ ParserConvinatorPaformance/Test.cs
@@ -0,0 +1,22 @@
+﻿using Marimo.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParserConbinatorParormance
+{
+    public class Test
+    {
+        [Fact(Skip ="必要なときのみ実行")]
+        public async Task パフォーマンス測定()
+        {
+            string text = File.ReadAllText(@"..\..\..\testdata\test.json");
+
+            await JSON.ParseAsync(text);
+        }
+    }
+}

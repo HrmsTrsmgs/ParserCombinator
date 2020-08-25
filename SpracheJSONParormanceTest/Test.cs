d20ca0c04880ee7f5c9e6170d0eec49509db20c6
新田光宏 <potimarimo@gmail.com>
2020-08-26 06:59:19 +0900

    SpracheJSONParormanceTestの作成

---
+++ SpracheJSONParormanceTest/Test.cs
@@ -0,0 +1,22 @@
+﻿using SpracheJSON;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpracheJSONParormanceTest
+{
+    public class Test
+    {
+        [Fact]
+        public void パフォーマンス測定()
+        {
+            string text = File.ReadAllText(@"..\..\testdata\test.json");
+
+            JSON.Parse(text);
+        }
+    }
+}

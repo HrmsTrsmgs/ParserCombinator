702c8248fb7eff5bcd6e1f39390a68c052f5a600
新田光宏 <potimarimo@gmail.com>
2020-09-12 14:52:15 +0900

    ConditionalCharParserの追加

---
+++ ParserCombinator/Core/ConditionalCharParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marimo.ParserCombinator.Core
+{
+    public class ConditionalCharParser : Parser<char>
+    {
+        Func<char, bool> Condition { get; }
+
+        public ConditionalCharParser(Func<char, bool> condition)
+        {
+            Condition = condition;
+        }
+
+        protected override (bool isSuccess, Cursol cursol, char parsed) ParseCore(Cursol cursol)
+            => cursol.Current switch
+            {
+                var c when (Condition(c))
+                        => (true, cursol.GoFoward(1), c),
+                _ => (false, cursol, default)
+            };
+    }
+}
---
+++ ParserCombinatorのテスト/Core/ConditionalCharParserのテスト.cs
@@ -0,0 +1,25 @@
+﻿using Marimo.ParserCombinator;
+using Marimo.ParserCombinator.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Marimo.Test.ParserCombinator.Core
+{
+    public class ConditionalCharParserのテスト
+    {
+        [Fact]
+        public void ParseAsyncは指定した文字を読み込みに成功します()
+        {
+            var cursol = new Cursol("public");
+            var tested = new ConditionalCharParser(c => c == 'p');
+
+            var result = tested.Parse(cursol);
+
+            result.isSuccess.IsTrue();
+        }
+
+    }
+}
